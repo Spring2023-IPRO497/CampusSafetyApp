884395dc5ffab961573bb8d575e7b20ca4d2ae06
verica-kar <vericakaranakova@gmail.com>
2023-04-18 16:25:59 -0500

    logout

--- app/src/main/java/com/campussafetyapp/iitridesafe/SuspiciousActivity.java
+++ app/src/main/java/com/campussafetyapp/iitridesafe/SuspiciousActivity.java
@@ -1,11 +1,14 @@
 package com.campussafetyapp.iitridesafe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
